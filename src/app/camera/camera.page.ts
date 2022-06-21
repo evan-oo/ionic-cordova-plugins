f08733d31dbc628c41c008ef4406bc62f3c74493
user <user@i.local>
2022-06-21 17:02:14 +0800

    Show Image Failed

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -10,6 +10,8 @@ import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 export class CameraPage implements OnInit {
   
   public image: string;
+  alt = "alt";
+  display = false;
 
   constructor(private camera: Camera) { }
 
@@ -28,9 +30,11 @@ export class CameraPage implements OnInit {
       this.camera.getPicture(options).then((imageData) => {
        // imageData is either a base64 encoded string or a file URI
        // If it's base64 (DATA_URL):
-       console.log("success takepic()");
-       let base64Image = 'data:image/jpeg;base64,' + imageData;
-      this.image = base64Image;
+        console.log("success takepic()");
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+        this.image = base64Image;
+        this.display = true;
+        console.log(this.image)
       }, (err) => {
        // Handle error
        alert(err);
